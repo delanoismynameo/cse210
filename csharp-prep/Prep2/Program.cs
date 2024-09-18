74ac49dc89a1c4d38a1a4f7416056ee69efd22b5
delanoismynameo <ste20059@byui.edu>
2024-09-17 22:23:04 -0600

    9/17/24 22:22

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade precentage? ");
+        string gradeString = Console.ReadLine();
+        int gradeInt = int.Parse(gradeString);
+        String letter = ("");
+        if (gradeInt >= 90)
+        {
+            letter = ("A");
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = ("B");
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = ("C");
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = ("D");
+        }
+        else if (gradeInt <= 60)
+        {
+            letter = ("F");
+        }
+        else
+        {
+            Console.Write("");
+        }
+        Console.WriteLine($"You have a(n) \"{letter}\" grade.");
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you hav failed the class. You were almost there! ");
+        }
     }
 }
