7e7ec35005584284b849a96df9218f5f11813f64
delanoismynameo <ste20059@byui.edu>
2024-09-19 14:15:04 -0600

    9/19/24 14:14

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int guessInt = -1;
+        int magicNumber = random.Next(0,101);
+
+        while (guessInt != magicNumber)
+        {
+            Console.Write("What is the magic number? ");
+            String guessString = Console.ReadLine();
+            guessInt = int.Parse(guessString);
+
+            if (guessInt > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessInt < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessInt == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.Write("");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+        int number = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+            string numberStr = Console.ReadLine();
+            number = int.Parse(numberStr);
+            numberList.Add(number);
+        }
+        int sum = numberList.Sum();
+        double mean = numberList.Average();
+        int max = numberList.Max();
+        Console.WriteLine($"The Sum is: {sum}");
+        Console.WriteLine($"The Average is: {mean}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
