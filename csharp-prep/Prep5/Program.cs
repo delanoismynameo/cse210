1427babb39a488a290df14ea00e32589da30876f
delanoismynameo <ste20059@byui.edu>
2024-09-20 13:31:04 -0600

    9/20/24 13:30

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        String name = PromptUserName();
+        int favoriteNumberInt = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumberInt);
+        DisplayResult(name, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static String PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        String name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumberString = Console.ReadLine();
+        int favoriteNumberInt = int.Parse(favoriteNumberString);
+        return favoriteNumberInt;
+    }
+
+    static int SquareNumber(int unsquaredNumber)
+    {
+       int squaredNumber = unsquaredNumber * unsquaredNumber;
+       return squaredNumber;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
