25fa8a5e494966c83065f5dd4e120bebf9515993
delanoismynameo <ste20059@byui.edu>
2024-09-24 16:28:59 -0600

    9/24/24 16:28

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System;
+public class Job
+{
+    public String _company;
+    public String _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJob()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
